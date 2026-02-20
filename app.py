5409e08d76c208821b12cbda98d3c04e69828af8
wagner-sm <wsmmetal@outlook.com>
2026-02-20 08:09:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -169,12 +169,4 @@ def exportar_produtos_xlsx():
     )
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=5000)
-        db.create_all()  
-        port = int(os.environ.get("PORT", 5000))  
-        app.run(host="0.0.0.0", port=port)
-
 
